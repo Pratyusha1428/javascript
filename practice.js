3e72f97e438f5a19c7e075b4c900ccb680083066
Pratyusha1428 <pratyusharai542@gmail.com>
2026-08-21 19:10:56 +0000

    Add javascript practice code

---
+++ practice.js
@@ -0,0 +1,94 @@
+const name = "Pratyusha";
+let age =18;
+const isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+let marks=75;
+if (marks>=40){
+    console.log("pass")
+}else{
+    console.log("fail")
+}
+
+let name1="pratyusha";
+let age1=18;
+let college1="GITM";
+let isStudent1=true;
+
+console.log(name1);
+console.log(age1);
+console.log(college1);
+console.log(isStudent1);
+
+let a=25
+let b=4
+let add=a+b;
+let sub=a-b;
+let product=a*b;
+let divide=a/b;
+let remainder =a%b
+console.log(add);
+console.log(sub);
+console.log(product);
+console.log(divide);
+console.log(remainder);
+
+let age2=20
+if(age2>=18){
+    console.log("eligible to vote")
+}else{
+     console.log("not eligible to vote")
+    };
+
+let num=17
+if(num%2===0){
+    console.log("even")
+}else{
+    console.log("odd")
+};
+
+let marks2=36
+if(marks2>=40){
+    console.log("pass")
+}else{
+    console.log("fail")
+};
+
+let isLoggedIn=true
+if(isLoggedIn===true){
+    console.log("welcome to campus connect")
+}else{
+    console.log("please login first")
+};
+
+let age_=19
+if(age_>=18){
+    console.log("you can apply for internship")
+}else{
+    console.log("you cannot apply for this internship")
+};
+
+let jobType="internship"
+if(jobType==="internship"){
+    console.log("this is an internship opportunity")
+}else{
+    console.log("this is a full time job")
+};
+
+let marks_=82
+if(marks>=80){
+    console.log("excellent")
+}else{
+    console.log("keep improving")
+};
+
+let age3=18
+hasId=true
+if(age3===18 && hasId===true ){
+    console.log("you can enter")
+}else{
+    console.log("you cannot enter")
+};
