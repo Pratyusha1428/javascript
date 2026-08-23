c1cafcb19586a2e48acab9976ccb2f3e6b4fe74d
Pratyusha1428 <pratyusharai542@gmail.com>
2026-08-23 17:59:34 +0000

    day 3 of learning Javascript

---
+++ practice3.js
@@ -0,0 +1,80 @@
+for(let i=10;i>=1;i--){
+    console.log(i)
+}
+
+for(let j=2;j<=20;j+=2){
+    console.log(j)
+}
+
+for(let k=1;k<=19;k+=2){
+    console.log(k)
+}
+
+for(let a=1;a<=20;a++){
+    if(a%3==0){
+        console.log(a,"number is divisible by 3 ")
+    }
+}
+
+for(let b=5;b<=50;b+=5){
+    console.log(b)
+}
+
+for(let c=1;c<=10;c++){
+    console.log(5*c)
+}
+
+let d=10;
+while(d>=1){
+    console.log(d)
+    d--
+}
+
+e=2;
+while(e<=20){
+    console.log(e)
+    e+=2
+}
+
+for(f=1;f<=20;f++){
+    if(f%4==0){
+        console.log(f,"is divisible by 4")
+    }
+}
+
+g=19;
+while(g>=10){
+    console.log(g)
+    g-=2
+}
+
+h=1;
+while(h<=10){
+    if(h===7){
+        break
+    }
+    console.log(h)
+    h+=1
+}
+
+let x=1;
+let s=0;
+while(x<=10){
+    (s+=x);
+    x++;
+}
+console.log(s)
+
+y=50;
+while(y>=40){
+    console.log(y)
+    y--
+}
+
+let z=2;
+let v=0;
+while(z<=20){
+    v+=z;
+    z+=2;
+}
+console.log(v)
